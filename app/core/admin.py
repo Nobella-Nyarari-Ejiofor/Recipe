501ac51e6bd4f4cf3b218c4ad61d1dbb5c3b5201
Nobella-Nyarari-Ejiofor <nobellanyarari@gmail.com>
2022-07-20 07:20:57 +0300

    created the users page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,6 @@
-# noqa :
+# flake8: noqa
 """
-django admin customisation 
+django admin customisation
 """
 from django.contrib import  admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
