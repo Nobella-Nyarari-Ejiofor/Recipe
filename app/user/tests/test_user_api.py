76b54961fc0bba8ce50aa9dc20135faa311748db
Nobella-Nyarari-Ejiofor <nobellanyarari@gmail.com>
2022-07-24 20:11:35 +0300

    Added views and test for the tokens

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -126,6 +126,7 @@ class PublicUserApiTests(TestCase):
 
         res = self.client.post(TOKEN_URL, payload)
         self.assertNotIn('token', res.data)
+        
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
 
