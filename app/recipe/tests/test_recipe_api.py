802ea98ae36540fa40fccf27fd73e4a69293d303
Nobella-Nyarari-Ejiofor <nobellanyarari@gmail.com>
2022-07-26 23:50:12 +0300

    Created test for the recipe detail api

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -8,10 +8,25 @@ from django.test import TestCase
 from django.urls import reverse
 
 from core.models import Recipe
+from rest_framework.test import APIClient 
+from rest_framework import status
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import (
+    RecipeSerializer,
+    RecipeDetailSerializer,
+)
+
 
 RECIPES_URL = reverse('recipe:recipe-list')
+
+def detail_url(recipe_id):
+    """
+    CReate and return a recipe detail URL
+    """
+    return reverse('recipe:recipe-detail', args = [recipe_id])
+
+
+
 def create_recipe(user, **params):
     """
     Create and return a sampple recipe
@@ -90,6 +105,18 @@ class PrivateRecipeApiTests(TestCase):
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code,status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipe_detail(self):
+        """
+        Test and get recipe detail
+        """
+        recipe = create_recipe(user = self.user)
+        url = detail_url(recipe.id)
+
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(res.data , serializer.data)
 
 
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,7 +3,7 @@ Views for the recipe api's
 """
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permossions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 
 from core.models import Recipe
 from recipe import serializers
@@ -23,4 +23,3 @@ class RecipeViewSet(viewsets.ModelViewSet):
         REtrieve recipe for authenticated user
         """
         return self.queryset.filter(user=self.request.user).order_by('-id')
-        
