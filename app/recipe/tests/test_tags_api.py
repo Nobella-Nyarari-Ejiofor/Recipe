74555d534cc596d899ba73737c12f8a2101bc296
Nobella-Nyarari-Ejiofor <nobellanyarari@gmail.com>
2022-08-08 15:37:58 +0300

    Test for deletiing a tag

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -84,7 +84,7 @@ class PrivateTagsApiTest(TestCase):
         self.assertEqual(len(res.data),1)
         self.assertEqual(res.data[0]['name'],tag.name)
     
-    def test_uodate_tag(self):
+    def test_update_tag(self):
         """
         Test updating a tag
         """
@@ -99,3 +99,16 @@ class PrivateTagsApiTest(TestCase):
         self.assertEqual(res.status_code , status.HTTP_200_OK)
         tag.refresh_from_db()
         self.assertEqual(tag.name , payload['name'])
+
+
+    def test_delete_tag(self):
+        """
+        Test deleting a tag
+        """
+        tag = Tag.objects.create(user =self.user ,name = 'Chinese Cuisines')
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code , status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user = self.user)
+        self.assertFalse(tags.exists())
