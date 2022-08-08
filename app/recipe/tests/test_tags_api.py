45618670143ce92d3c24d3322269bb92a2018fc9
Nobella-Nyarari-Ejiofor <nobellanyarari@gmail.com>
2022-08-08 14:12:51 +0300

    Created tests for the tags api

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -50,5 +50,31 @@ class PrivateTagsApiTest(TestCase):
 
     def test_retrieve_tags(self):
         """
+        Test for retrieving a likst of tags
         """
+        Tags.objects.create(user = self.user ,name = 'Vegan')
+        Tags.objects.create(user = self.user , name = 'Fruity')
+
+        res = self.cient.get(TAGS_URL)
+
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags , many = True)
+
+        self.assertEqual(res.status_code , status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+
+        """
+        Test that listt oof tags is limited to authenticated user
+        """
+        user2 = create_user(email = 'example2.com' , password ='example12')
+        Tag.objects.create(user = user2 , name = 'Fruity')
+        tag = Tag.objects.create(user =self.user , name ='Comfort Food')
+
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data),1)
+        self.assertEqual(res.data[0]['name'],tag.name)
 
