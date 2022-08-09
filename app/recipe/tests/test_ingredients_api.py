1e27c897d1e67ffa9d19b8ed5cd865603f1cc30b
Nobella-Nyarari-Ejiofor <nobellanyarari@gmail.com>
2022-08-09 22:31:47 +0300

    crated tests for listing ingredients API {

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
 
-from test_framework import status
+from rest_framework import status
 from rest_framework.test import APIClient
 
 from core.models import models
@@ -39,3 +39,50 @@ class PublicIngredientsAPITests(TestCase):
         res = self.client.get(INGREDIENTS_URL)
 
         self.assertEqual(res.status_code , status.HTTP_401_UNAUTHORIZED)
+
+class PrivateIngredientsAPITests(TestCase):
+    """
+    Test Authorized API requests
+    """
+
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredients(self):
+        """
+        Test retrieving a list of ingredients
+        """
+        Ingredient.objects.create(user = self.user , name = 'Tomato')
+        Ingredient.objects.create(user = self.user, name = 'Onion')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        ingredients =  Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many = True)
+
+        self.assertEqual(res.status_code , status.HTTP_200_OK)\
+        self.assertEqual(res.data, serializer.data)
+
+
+    def test_ingredients_limited_to_user(self):
+        """
+        Test list of ingredients is limited to the user
+        """
+        user2 = create_user(email = "example2@me.com")
+        Ingredient.objects.create(user = user2 , name ='Salt')
+
+        ingredient =Ingredient.objects.create(user = self.user , name = 'Pepper')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data),1)
+        self.assertEqual(res.data[0]['name'],ingredient.name)
+        self.assertEqual(res.data[0]['id'], ingredient.id)
+
+        
+
+
+
